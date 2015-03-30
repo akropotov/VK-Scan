b2ffeb6d7174c21a935890e4c2e3b310ff5015fc
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2015-03-30 16:42:24 +0300

    Тест

--- js/main.js
+++ js/main.js
@@ -442,14 +442,14 @@ function comments_get() {
         users_ids = array_count_values(users_ids);
         for (var id in users_ids) {
             u_id = (id < 0) ? 101 : parseInt(id, 10);
-            console.log('1:' + u_id + ' / ' + id);
             users_obj[u_id] = { id: u_id, count: users_ids[id], likes: 0 };
         }
+        console.log('1:' + users_obj);
         comments.map(function(comment) {
             u_id = (comment.from_id < 0) ? 101 : parseInt(comment.from_id, 10);
-            console.log('2:' + u_id + ' / ' + comment.from_id);
             users_obj[u_id].likes += comment.likes;
         });
+        console.log('2:' + users_obj);
         for (var id in users_obj) {
             users.push({ id: users_obj[id].id, count: users_obj[id].count, likes: users_obj[id].likes });
         }
